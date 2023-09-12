533f40296b482133426c3145fbaba4a0911915d3
DylBrad <dylanbradley1@hotmail.com>
2023-09-12 19:48:52 +0100

    Style map

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,28 @@
 'use client';
 import * as React from 'react';
 import styles from './page.module.css';
-import Map from 'react-map-gl';
+import Map, {
+  Marker,
+  Popup,
+  GeolocateControl,
+  NavigationControl,
+} from 'react-map-gl';
 
 export default function Home() {
   const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+  const [viewState, setViewState] = React.useState({
+    longitude: -6.2603,
+    latitude: 53.3498,
+    zoom: 11,
+  });
 
   return (
     <Map
+      {...viewState}
       mapboxAccessToken={mapboxToken}
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14,
-      }}
-      style={{ width: 600, height: 400 }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
+      style={{ marginLeft: '260px', width: 'auto', height: '100vh' }}
+      onMove={(evt) => setViewState(evt.viewState)}
+      mapStyle="mapbox://styles/dylbrad/cl9h7i0r900it14pi0yg2sacm"
     />
   );
 }
