e58b16c6fa7bff1625136ac8aa8f10700555fbb1
DylBrad <dylanbradley1@hotmail.com>
2023-09-13 21:17:14 +0100

    Revert "API bug fix testing"
    This reverts commit ea1d4ed727074a5d0533a0612dbab9cc9f23ce4b.

--- src/app/page.js
+++ src/app/page.js
@@ -19,8 +19,6 @@ export default function Home() {
     zoom: 11,
   });
 
-  console.log(proccess.env.NEXT_PUBLIC_API_URL);
-
   const getAllMarkers = async () => {
     const logEntries = await listLogEntries();
     setLogEntries(logEntries);
