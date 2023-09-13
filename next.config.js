2e19f37ff7aafdb3c0caa77f831024b32f788455
DylBrad <dylanbradley1@hotmail.com>
2023-09-13 21:07:40 +0100

    API bug fix testing

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: `${process.env.NEXT_PUBLIC_BASE_PATH}/` || '',
 };
 
 module.exports = nextConfig;
