23e1d6793fbe83e12dd0ac361bba67a0cbc35f37
DylBrad <dylanbradley1@hotmail.com>
2023-09-17 11:15:37 +0100

    Add new profile post form

---
+++ src/app/components/NewPostForm/NewPostForm.js
@@ -0,0 +1,102 @@
+'use client';
+
+import * as React from 'react';
+import { useForm } from 'react-hook-form';
+import { createUserPost } from '../../API';
+import { useCookies } from 'react-cookie';
+import jwt_decode from 'jwt-decode';
+import { IconContext } from 'react-icons';
+import { MdImageSearch } from 'react-icons/md';
+
+const NewPostForm = (props) => {
+  const [imageUrl, setImageUrl] = React.useState(null);
+  const { register, handleSubmit } = useForm();
+  const [cookies] = useCookies(['user']);
+
+  const token = cookies.token;
+  let decodedToken = undefined;
+  if (token !== undefined) {
+    decodedToken = jwt_decode(token);
+  }
+
+  const uploadImage = async () => {
+    const data = new FormData();
+    data.append('file', imageUrl);
+    data.append('upload_preset', 'post_images');
+    data.append('cloud_name', process.env.NEXT_PUBLIC_CLOUDINARY_NAME);
+    data.append('folder', 'profile_and_newsfeed_post_pictures');
+    const posting = await fetch(process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_URL, {
+      method: 'post',
+      body: data,
+    });
+    try {
+      const response = await posting.json();
+      console.log('URL: ', response.url);
+      return response.url;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onSubmit = async (data) => {
+    const cloudinaryImgUrl = await uploadImage();
+
+    data.author = decodedToken.userId;
+    data.image = cloudinaryImgUrl;
+    await createUserPost(data);
+  };
+
+  const handleClick = () => {
+    props.setShowNewPostForm(false);
+  };
+
+  return (
+    <div className="fullscreen-form-container">
+      <div className="map-form-wrapper center-form form-container">
+        <form
+          className="entry-form edit-profile-form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="close-icon" onClick={handleClick}>
+            ✖
+          </div>
+
+          <div className="wrap-input">
+            <label htmlFor="img-select" className="form-file-input-wrapper">
+              Select Image
+              <div className="form-input type-file">
+                <IconContext.Provider
+                  value={{ className: 'react-icons', size: 20 }}
+                >
+                  <MdImageSearch value={{ className: 'react-icons' }} />
+                  <span>Upload from device</span>
+                </IconContext.Provider>
+              </div>
+              <input
+                type="file"
+                onChange={(e) => {
+                  setImageUrl(e.target.files[0]);
+                }}
+                id="img-select"
+              ></input>{' '}
+            </label>
+          </div>
+
+          <div className="wrap-input">
+            <label htmlFor="description">description</label>
+            <textarea
+              {...register('description')}
+              rows="3"
+              className="form-input form-input-txtarea"
+              placeholder="Write something about yourself"
+            ></textarea>
+          </div>
+
+          <button className="primary-button form-button">Create Post</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default NewPostForm;
---
+++ src/app/profile/page.js
@@ -0,0 +1,143 @@
+'use client';
+import * as React from 'react';
+import { useCookies } from 'react-cookie';
+
+import { IconContext } from 'react-icons';
+import { FaUserAlt } from 'react-icons/fa';
+import { GrAddCircle } from 'react-icons/gr';
+
+import jwt_decode from 'jwt-decode';
+
+import NewPostForm from '../components/NewPostForm/NewPostForm';
+import DeleteButton from '../components/DeleteButton/DeleteButton';
+import EditProfileForm from '../components/EditProfileForm/EditProfileForm';
+import Nav from '../components/Nav/Nav';
+
+import { listCurrentUserPosts, findOneUser } from '../API';
+
+const Profile = () => {
+  const [showNewPostForm, setShowNewPostForm] = React.useState(false);
+  const [showEditProfileForm, setShowEditProfileForm] = React.useState(false);
+  const [posts, setPosts] = React.useState([]);
+  const [profilePic, setProfilePic] = React.useState(null);
+  const [username, setUsername] = React.useState(null);
+  const [cookies] = useCookies(['user']);
+
+  const token = cookies.token;
+
+  let decodedToken = undefined;
+  if (token !== undefined) {
+    decodedToken = jwt_decode(token);
+  }
+
+  const getUsersPosts = async () => {
+    // const posts = await listPosts();
+    const id = decodedToken.userId;
+    const posts = await listCurrentUserPosts(id);
+    setPosts(posts);
+  };
+
+  const getUserProfilePic = async () => {
+    const id = decodedToken.userId;
+    const user = await findOneUser(id);
+    const profilePicture = user.profile_pic;
+    setProfilePic(profilePicture);
+    const userName = user.username;
+    setUsername(userName);
+  };
+
+  React.useEffect(() => {
+    getUsersPosts();
+    getUserProfilePic();
+    // eslint-disable-next-line
+  }, []);
+
+  const handleClick = () => {
+    setShowNewPostForm(true);
+  };
+
+  // edit profile form
+  const handleShowEditProfileForm = () => {
+    setShowEditProfileForm(true);
+  };
+
+  return (
+    <>
+      <Nav></Nav>
+      <div className="nav-child profile-container">
+        <div className="profile-info">
+          <div className="profile-pic">
+            {profilePic ? (
+              <div className="profile-pic-container">
+                <img alt="" src={profilePic}></img>
+              </div>
+            ) : (
+              <IconContext.Provider
+                value={{ className: 'react-icons', size: 54 }}
+              >
+                <FaUserAlt value={{ className: 'react-icons' }} />
+              </IconContext.Provider>
+            )}
+          </div>
+          <div className="profile-bio">
+            <h2>{username}</h2>
+            <p>
+              {decodedToken.bio
+                ? decodedToken.bio
+                : `Hello, I am ${username}! Lets connect!`}
+            </p>
+            <div>
+              <button
+                className="edit-profile-btn"
+                onClick={handleShowEditProfileForm}
+              >
+                Edit Profile
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="button-container" onClick={handleClick}>
+          <IconContext.Provider value={{ className: 'react-icons', size: 64 }}>
+            <GrAddCircle />
+          </IconContext.Provider>
+          <span>New Post</span>
+        </div>
+
+        <div className="profile-gallery">
+          {posts.map((post) => {
+            return (
+              <div className="image-box">
+                <div
+                  className="grid-image"
+                  style={{ backgroundImage: 'url(' + post.image + ')' }}
+                ></div>
+                <div className="overlay">
+                  <div className="details">
+                    <h2>{post.description}</h2>
+                  </div>
+                  <DeleteButton postId={post._id} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+        {(showNewPostForm || showEditProfileForm) && (
+          <>
+            {showNewPostForm && (
+              <NewPostForm setShowNewPostForm={setShowNewPostForm} />
+            )}
+            {showEditProfileForm && (
+              <EditProfileForm
+                setShowEditProfileForm={setShowEditProfileForm}
+              />
+            )}
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Profile;
